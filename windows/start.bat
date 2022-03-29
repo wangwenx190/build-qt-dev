82d11a5a68721f3be582a65a6c223271e2bd6239
Yuhang Zhao <2546789017@qq.com>
2022-03-29 20:43:18 +0800

    wip
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- windows/start.bat
+++ windows/start.bat
@@ -25,7 +25,7 @@ setlocal
 :: Modify this line to suit your own personal needs.
 :: Be careful about the dependencies of each repository.
 :: qtbase should always be the first one to build.
-set __qt_modules=qtbase,qtshadertools,qtimageformats,qtlanguageserver,qtsvg,qtdeclarative,qt5compat,qtremoteobjects,qtmultimedia,qtscxml,qtlottie,qtwebsockets,qtwebchannel,qtwebengine
+set __qt_modules=qtbase,qtshadertools,qtimageformats,qtlanguageserver,qtsvg,qtdeclarative,qt5compat,qtremoteobjects,qtmultimedia,qtscxml,qtlottie,qtwebsockets,qtwebchannel,qtwebengine,qtwebview
 :: Supported values: clang-cl, mingw and msvc
 set __compiler=clang-cl
 :: Supported values: x64, x86 and arm64
