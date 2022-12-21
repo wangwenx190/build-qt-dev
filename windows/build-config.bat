dc601c16588ecde80ce74273c2a9824b9a460450
Yuhang Zhao <2546789017@qq.com>
2022-12-21 20:38:42 +0800

    adjust modules
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- windows/build-config.bat
+++ windows/build-config.bat
@@ -26,8 +26,9 @@
 :: qtbase should always be the first one to build.
 :: We need the QtTools module for its linguist tools,
 :: they are necessary for applications that support i18n.
+:: Temporarily removed qtmultimedia: can't build FFmpeg in vcpkg.
 :: Temporarily removed qtwebengine: doesn't work well with VC-LTL.
-set __qt_modules=qtbase,qtshadertools,qtimageformats,qtlanguageserver,qtsvg,qttools,qtdeclarative,qt5compat,qtmultimedia,qthttpserver,qtvirtualkeyboard,qtdatavis3d,qtcharts,qtlottie,qtquicktimeline,qtactiveqt,qtremoteobjects,qt3d,qtquick3d,qtspeech,qttranslations
+set __qt_modules=qtbase,qtshadertools,qtimageformats,qtlanguageserver,qtsvg,qttools,qtdeclarative,qt5compat,qtquick3d,qtquickeffectmaker,qthttpserver,qtactiveqt,qttranslations
 :: Supported values: clang-cl, mingw and msvc
 set __compiler=msvc
 :: Supported values: x64, x86 and arm64
